d5176dffd21d9495dec5ae454091b656b1ac3e66
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2023-11-15 17:40:18 -0300

    Add files via upload

--- projects/store/mobile.js
+++ projects/store/mobile.js
@@ -10,24 +10,31 @@ const bfFrame = document.querySelector(".blackflag");
 const mtFrame = document.querySelector(".metallica");
 
 const aside1 = document.querySelector(".aside1");
+const shirtName = document.querySelector(".asideshirtname");
+const selectedShirt = document.querySelector(".selected-tshirt");
+
+let shirtColor = 0;
 
 function changeColorBFBlack () {
+    shirtColor = 0;
+    if (shirtColor === 0) {
     bfFrame.style.backgroundImage = "url('img/blackflag_black.jpg')";
+    };
 }
+
 function changeColorBFWhite () {
-    bfFrame.style.backgroundImage = "url('img/blackflag_white.jpg')";
-}
+    shirtColor = 1
+    if (shirtColor === 1) {
+      bfFrame.style.backgroundImage = "url('img/blackflag_white.jpg')";
+    }
+};
 
 function changeColorMTBlack () {
     mtFrame.style.backgroundImage = "url('img/metallica_black.jpg')";
 }
 function changeColorMTCharcoal () {
     mtFrame.style.backgroundImage = "url('img/metallica_charcoal.jpg')";
-}
-
-function bfBagOpen () {
-    aside1.style.transform = "translateX(0%)";
-}
+};
 
 bfBlack.addEventListener("click", changeColorBFBlack);
 bfWhite.addEventListener("click", changeColorBFWhite);
@@ -35,4 +42,16 @@ bfWhite.addEventListener("click", changeColorBFWhite);
 mtBlack.addEventListener("click", changeColorMTBlack);
 mtCharcoal.addEventListener("click", changeColorMTCharcoal);
 
-bfBag.addEventListener("click", bfBagOpen);
+bfBag.addEventListener("click", function () {
+  let bag = {
+    shirtBag: shirtColor,
+  };
+  console.log(bag.shirtBag);
+  aside1.style.transform = "translateX(0%)";
+
+  if (bag.shirtBag === 0) {
+    selectedShirt.style.backgroundImage = "url('img/blackflag_black.jpg')";
+  } else {
+    selectedShirt.style.backgroundImage = "url('img/blackflag_white.jpg')";
+  }
+});

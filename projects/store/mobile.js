7ed2f73e74f9a04f190ecf75d8b4389cae219a95
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2023-11-15 13:07:35 -0300

    Add files via upload

--- projects/store/mobile.js
+++ projects/store/mobile.js
@@ -4,9 +4,12 @@ const bfBlack = document.querySelector(".bf-black");
 const bfWhite = document.querySelector(".bf-white");
 const mtBlack = document.querySelector(".mt-black");
 const mtCharcoal = document.querySelector(".mt-charcoal");
+const bfBag = document.querySelector(".bf-bag");
 
 const bfFrame = document.querySelector(".blackflag");
 const mtFrame = document.querySelector(".metallica");
+
+const aside1 = document.querySelector(".aside1");
 
 function changeColorBFBlack () {
     bfFrame.style.backgroundImage = "url('img/blackflag_black.jpg')";
@@ -22,8 +25,14 @@ function changeColorMTCharcoal () {
     mtFrame.style.backgroundImage = "url('img/metallica_charcoal.jpg')";
 }
 
+function bfBagOpen () {
+    aside1.style.transform = "translateX(0%)";
+}
+
 bfBlack.addEventListener("click", changeColorBFBlack);
 bfWhite.addEventListener("click", changeColorBFWhite);
 
 mtBlack.addEventListener("click", changeColorMTBlack);
 mtCharcoal.addEventListener("click", changeColorMTCharcoal);
+
+bfBag.addEventListener("click", bfBagOpen);

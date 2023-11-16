20effa472fd7848bfaf18c976269bd68639a19d5
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2023-11-15 21:18:59 -0300

    Add files via upload

--- projects/store/mobile.js
+++ projects/store/mobile.js
@@ -4,7 +4,12 @@ const bfBlack = document.querySelector(".bf-black");
 const bfWhite = document.querySelector(".bf-white");
 const mtBlack = document.querySelector(".mt-black");
 const mtCharcoal = document.querySelector(".mt-charcoal");
+
 const bfBag = document.querySelector(".bf-bag");
+const mtBag = document.querySelector(".mt-bag");
+
+const bag = document.querySelectorAll(".blah");
+
 
 const bfFrame = document.querySelector(".blackflag");
 const mtFrame = document.querySelector(".metallica");
@@ -12,6 +17,7 @@ const mtFrame = document.querySelector(".metallica");
 const aside1 = document.querySelector(".aside1");
 const shirtName = document.querySelector(".asideshirtname");
 const selectedShirt = document.querySelector(".selected-tshirt");
+const cancelBtn = document.querySelector(".cancel");
 
 let shirtColor = 0;
 
@@ -30,10 +36,16 @@ function changeColorBFWhite () {
 };
 
 function changeColorMTBlack () {
+  shirtColor = 0;
+  if (shirtColor === 0) {
     mtFrame.style.backgroundImage = "url('img/metallica_black.jpg')";
+  };
 }
 function changeColorMTCharcoal () {
-    mtFrame.style.backgroundImage = "url('img/metallica_charcoal.jpg')";
+    shirtColor = 1
+    if (shirtColor === 1) {
+      mtFrame.style.backgroundImage = "url('img/metallica_charcoal.jpg')";
+    }
 };
 
 bfBlack.addEventListener("click", changeColorBFBlack);
@@ -42,16 +54,46 @@ bfWhite.addEventListener("click", changeColorBFWhite);
 mtBlack.addEventListener("click", changeColorMTBlack);
 mtCharcoal.addEventListener("click", changeColorMTCharcoal);
 
-bfBag.addEventListener("click", function () {
-  let bag = {
-    shirtBag: shirtColor,
-  };
-  console.log(bag.shirtBag);
-  aside1.style.transform = "translateX(0%)";
+for (let i = 0; i < bag.length; i++)
+  bag[i].addEventListener("click", function (event) {
 
-  if (bag.shirtBag === 0) {
-    selectedShirt.style.backgroundImage = "url('img/blackflag_black.jpg')";
+  let bandName;
+
+  if (event.target.classList.contains("bf-bag")) {
+    bandName = "black flag";
+    shirtName.textContent = `Black Flag T-Shirt`;
   } else {
+    bandName = "metallica";
+    shirtName.textContent = `Metallica T-Shirt`;
+  }
+
+  if (bandName === "black flag" && shirtColor === 0) {
+    selectedShirt.style.backgroundImage = "url('img/blackflag_black.jpg')";
+  } 
+  
+  if (bandName === "black flag" && shirtColor === 1) {
     selectedShirt.style.backgroundImage = "url('img/blackflag_white.jpg')";
   }
+
+  if (bandName === "metallica" && shirtColor === 0) {
+    selectedShirt.style.backgroundImage = "url('img/metallica_black.jpg')";
+  } 
+  
+  if (bandName === "metallica" && shirtColor === 1) {
+    selectedShirt.style.backgroundImage = "url('img/metallica_charcoal.jpg')";
+  }
+
+  console.log(bandName);
+  console.log(shirtColor)
+  
+  aside1.style.transform = "translateX(0%)";
 });
+
+function cancel() {
+  aside1.style.transform = "translateX(100%)";
+  shirtColor = 0;
+  bfFrame.style.backgroundImage = "url('img/blackflag_black.jpg')";
+  mtFrame.style.backgroundImage = "url('img/metallica_black.jpg')";
+}
+
+cancelBtn.addEventListener("click", cancel);
